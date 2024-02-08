b7e25c32cdc64a2bdb1ee05bd068d4b800b1db97
jungwoochenle <jungwoochenle@localhost>
2024-02-08 12:49:29 -0600

    Finish.

---
+++ src/studio3/Sieve.java
@@ -0,0 +1,24 @@
+package studio3;
+
+import java.util.Scanner;
+
+public class Sieve {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+
+		int[] numbers= new int[40];
+		for(int i=2; i<numbers.length;i++)
+		{
+			for(int j = i + i, i < 40, i + i)
+			
+				System.out.print(i);
+			}
+		}
+			
+		
+		
+
+	}
+
+}
